a9f0992ed8b490a9c8f00a3cfb82a1549d608d19
yjp900918 <yjp900918@gmail.com>
2018-04-01 11:59:10 -0400

    Fade in/out

---
+++ Assets/Button.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour {
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect (280, 300, 200, 20), "Click to Shift Scene"))
+        {
+            StartCoroutine(FadeScene());
+        }
+    }
+
+    IEnumerator FadeScene()
+    {
+        float time = GameObject.Find("Fade").GetComponent<FadeScene>().BeginFade(1);
+        yield return new WaitForSeconds(time);
+
+        SceneManager.LoadScene("Game");
+    }
+}
